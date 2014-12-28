c82c102330f179fd160c690ee065f28f0ef8aab5
Jared Norman <jared@freerunningtechnologies.com>
2014-12-27 20:53:59 -0800

    Add hex.pm metadata

--- mix.exs
+++ mix.exs
@@ -5,7 +5,15 @@ defmodule Dismake.Mixfile do
     [app: :dismake,
      version: "1.0.0",
      elixir: "~> 1.0",
-     deps: []]
+     deps: [],
+     package: [
+       contributors: ["Jared Norman"],
+       links: %{github: "https://github.com/jarednorman/dismake"},
+       licenses: ["MIT"]
+     ],
+     description: """
+     Dismake is a "compiler" (as in Mix.compilers) that just runs make.
+     """]
   end
 
   def application, do: [applications: []]
